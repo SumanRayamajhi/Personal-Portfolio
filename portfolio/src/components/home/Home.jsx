57b53fea22477fa58763c77fbd7e25aebc20e031
Suman Rayamajhi <sams.rayamajhi@gmail.com>
2023-11-10 01:45:55 +0100

    create home section

---
+++ portfolio/src/components/home/Home.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { FaAngleDown } from "react-icons/fa";
+import "./home.css";
+
+const Home = () => {
+  return (
+    <section className="home" id="Home">
+      <div className="home__container">
+        <div className="home__container-side-line">
+          <div className="home__container-circle" />
+          <div className="home__container-line" />
+        </div>
+        <div className="home__meta ">
+          <h2 className="home__text">
+            Hello, I'm <span>Suman Rayamajhi</span>
+          </h2>
+          <h3 className="home__text">Full Stack Web Developer </h3>
+          <button className="home__button">
+            More About Me
+            <span className="button__icon">
+              <FaAngleDown />
+            </span>
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
