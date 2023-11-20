937c77f6c7a314a5250c715491900e01155d3230
Suman Rayamajhi <sams.rayamajhi@gmail.com>
2023-11-20 01:03:41 +0100

    adjust contact

--- portfolio/src/components/contact/socialMedia/SocialMedia.jsx
+++ portfolio/src/components/contact/socialMedia/SocialMedia.jsx
@@ -1,31 +1,31 @@
 import React from "react";
-import { FaMobileAlt, FaAt, FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaMobileAlt, FaAt, FaGithub, FaLinkedinIn } from "react-icons/fa";
 import "./socialMedia.css";
 
 const SocialMedia = () => {
   return (
     <div className="socialMedia__Wrapper">
       <div className="socialMedia__Button">
-        <FaMobileAlt className="icon" />
+        <FaMobileAlt className="socialMedia__Button-icon" />
         <h3>Call Me</h3>
         <span>+34631887080</span>
       </div>
 
       <div className="socialMedia__Button">
-        <FaAt className="icon" />
+        <FaAt className="socialMedia__Button-icon" />
         <h3>Send Me an Email</h3>
         <span>sams.rayamajhi@gmail.com</span>
       </div>
 
       <div className="socialMedia__Button">
-        <FaGithub className="icon" />
+        <FaGithub className="socialMedia__Button-icon" />
         <h3>My Github</h3>
         <span>
           <a href="https://github.com/SumanRayamajhi/">Github</a>
         </span>
       </div>
       <div className="socialMedia__Button">
-        <FaLinkedin className="icon" />
+        <FaLinkedinIn className="socialMedia__Button-icon" />
         <h3>My Linkedin</h3>
         <span>
           <a href="https://www.linkedin.com/in/suman-rayamajhi/">Linkedin</a>
